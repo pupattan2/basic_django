ad9f3f4ff1b79be451fa03fbc14d5fa19fafc5ca
pupattan <pkbsdmp@gmail.com>
2018-02-24 20:40:21 +0530

    updating req

--- startapp/settings.py
+++ startapp/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'  	
 ]
 
 ROOT_URLCONF = 'startapp.urls'
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
