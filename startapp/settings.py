1a5572690c25d67771a7cf22f55f7a674e95fb40
pupattan <pkbsdmp@gmail.com>
2018-02-24 21:17:19 +0530

    starting

--- startapp/settings.py
+++ startapp/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'startapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
+    'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    }
 }
 import dj_database_url
 db_from_env = dj_database_url.config()
